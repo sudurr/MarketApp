3e7bad5eb4f166592a42276ed6cb96c9df5d6663
sudurr <sudursugunushev@gmail.com>
2024-01-21 20:01:05 +0300

    have been created card model and started network service

---
+++ MarketApp/Models/Cards.swift
@@ -0,0 +1,22 @@
+//
+//  Cards.swift
+//  MarketApp
+//
+//  Created by Судур Сугунушев on 21.01.2024.
+//
+
+import Foundation
+
+typealias Cards = [Card]
+
+struct Card: Codable {
+    let id: Int
+    let image: String
+    let categories: Categories
+    let name, description: String
+}
+
+struct Categories: Codable {
+    let id: Int
+    let icon, image, name: String
+}
---
+++ MarketApp/Service/NetworkManager.swift
@@ -0,0 +1,18 @@
+//
+//  NetworkManager.swift
+//  MarketApp
+//
+//  Created by Судур Сугунушев on 21.01.2024.
+//
+
+import Foundation
+
+final class NetworkManager: NetworkProtocol {
+
+}
+
+
+
+protocol NetworkProtocol: AnyObject {
+
+}
